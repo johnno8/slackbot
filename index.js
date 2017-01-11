7010f329c37847ee405559aa1985a8a121403776
john o'keeffe <jokeeffe09@gmail.com>
2017-01-11 22:40:11 +0000

    First multi-stage conversation implemented

--- index.js
+++ index.js
@@ -13,9 +13,46 @@ controller.spawn({
 }).startRTM()
 
 // give the bot something to listen for.
+/*
 controller.hears('hello',['direct_message','direct_mention','mention'],function(bot,message) {
 
   console.log(JSON.stringify(message));
   bot.reply(message,'Hello yourself.');
 
+});*/
+
+controller.hears(['hello'], ['message_received','direct_message','direct_mention','mention'], function(bot,message) {
+    var askName = function(err, convo) {
+      convo.ask('Howya, what\'s your name?', function(response, convo) {
+        convo.say('Deadly, I\'m mebot');
+        askAge(response, convo);
+        convo.next();
+      });
+    };
+    var askAge = function(response, convo) {
+      convo.ask('What age are you?', function(response, convo) {
+        console.log(JSON.stringify(response));
+        convo.say('That old? Ok!')
+        if(!response.text.match(/^\d+$/)){
+          convo.stop();
+        }
+        askWhereFrom(response, convo);
+        convo.next();
+      });
+    };
+    var askWhereFrom = function(response, convo) {
+      convo.ask('Where are you from anyway?', function(response, convo) {
+        convo.say('Ah yeah, I know it well, nice place!');
+        convo.say('Talk to you later...');
+        convo.next();
+      });
+    };
+
+    bot.startConversation(message, askName);
 });
+
+
+
+
+
+
